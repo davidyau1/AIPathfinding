bca96bc5ad58a02ae2db822b8befa88d85ca3919
David Yau <davidyau1@outlook.com>
2022-05-17 17:12:10 +1000

    animations

---
+++ Assets/Scripts/LevelMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject door;
+    public GameObject boat;
+
+    // Update is called once per frame
+    public void MoveDoor()
+    {
+        
+    }
+    public void MoveBoat()
+    {
+
+    }
+     
+}
